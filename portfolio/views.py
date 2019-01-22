fd27e913f4265966cda8d18e2e3d1f4141b130f0
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-01-22 13:21:32 +0100

    Fix merge conflicts

--- portfolio/views.py
+++ portfolio/views.py
@@ -9,7 +9,7 @@ def main(menu=None):
     if isinstance(menu, str):
         menu = menu.upper()
     menu_items = ["MAIN", "PROJECTS", "BLOG", "ABOUT"]
-    return render_template("layout.html", menu_items=menu_items, current_menu=menu)
+    return render_template("frontpage/layout.html", menu_items=menu_items, current_menu=menu)
 
 
 if __name__=="__main__":
