17ae81b7a17e6e7c1133ddaed23773be6673c422
simen <simenberghansen97@gmail.com>
2019-03-04 16:11:02 +0100

    Add pictures, scroll and fix projects

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('portfolio/',views.index)
+    path('portfolio/',views.index),
+    path('pictures/', views.pictures)
 ]
