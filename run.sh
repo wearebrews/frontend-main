0be9156b448a3c701257673af7e13c7bc410a0d6
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-03-22 15:30:37 +0100

    Update run

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 #Collect static files
 python manage.py collectstatic
 
+#Check if there are migration changes
+python manage.py makemigrations
+
 #Start gunicorn server
 gunicorn wearebrews.wsgi --bind unix:/tmp/gunicorn.sock
